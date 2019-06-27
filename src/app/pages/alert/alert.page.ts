4e21b3a3f671f0275ba6dfbddc75590ba77f48bc
edorta <burdaich@gmail.com>
2019-06-27 19:46:35 +0200

    alertas con cambio de titulo

--- src/app/pages/alert/alert.page.ts
+++ src/app/pages/alert/alert.page.ts
@@ -1,15 +1,46 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit } from "@angular/core";
+import { AlertController } from "@ionic/angular";
+import { threadId } from "worker_threads";
 @Component({
-  selector: 'app-alert',
-  templateUrl: './alert.page.html',
-  styleUrls: ['./alert.page.scss'],
+  selector: "app-alert",
+  templateUrl: "./alert.page.html",
+  styleUrls: ["./alert.page.scss"]
 })
 export class AlertPage implements OnInit {
+  titulo: string;
 
-  constructor() { }
+  constructor(public alertController: AlertController) {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  async presentAlertPrompt() {
+    const alert = await this.alertController.create({
+      header: "Prompt!",
+      inputs: [
+        {
+          name: "txtNombre",
+          type: "text",
+          placeholder: "titulo"
+        }
+      ],
+      buttons: [
+        {
+          text: "Cancel",
+          role: "cancel",
+          cssClass: "secondary",
+          handler: () => {
+            console.log("Confirm Cancel");
+          }
+        },
+        {
+          text: "Ok",
+          handler: (data) => {
+            this.titulo = data.txtNombre;
+          }
+        }
+      ]
+    });
+
+    await alert.present();
   }
-
 }
