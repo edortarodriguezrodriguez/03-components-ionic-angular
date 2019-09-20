a60aea3cf4fe379c6840500c423b174430ee8696
rodriguez <e.rodriguez@ariadnagrid.com>
2019-09-20 12:02:54 +0200

    ion-list-sliding

--- src/app/pages/list/list.page.ts
+++ src/app/pages/list/list.page.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { DataService } from '../../services/data.service';
 import { Observable } from 'rxjs';
+import { userInfo } from 'os';
+import { IonList } from '@ionic/angular';
 
 @Component({
   selector: 'app-list',
@@ -8,6 +10,7 @@ import { Observable } from 'rxjs';
   styleUrls: ['./list.page.scss'],
 })
 export class ListPage implements OnInit {
+  @ViewChild('list') list: IonList;
   usuarios: Observable<any>;
 
   constructor(private dataService: DataService) { }
@@ -16,4 +19,19 @@ export class ListPage implements OnInit {
     this.usuarios = this.dataService.getUser();
   }
 
+  favorite(user) {
+    console.log('favorite', user);
+    this.list.closeSlidingItems();
+  }
+
+  share(user) {
+    console.log('share', user);
+    this.list.closeSlidingItems();
+  }
+
+  delete(user) {
+    console.log('delete', user);
+    this.list.closeSlidingItems();
+
+  }
 }
