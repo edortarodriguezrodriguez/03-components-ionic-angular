df17ea01a06544b73a4267dd628c817d53fa856a
rodriguez <e.rodriguez@ariadnagrid.com>
2019-10-23 16:35:39 +0200

    Modal

---
+++ src/app/pages/modal-info/modal-info.page.ts
@@ -0,0 +1,28 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {ModalController} from '@ionic/angular';
+
+@Component({
+    selector: 'app-modal-info',
+    templateUrl: './modal-info.page.html',
+    styleUrls: ['./modal-info.page.scss'],
+})
+export class ModalInfoPage implements OnInit {
+
+    @Input() name;
+    @Input() country;
+
+    constructor(private modalCtrl: ModalController) {
+    }
+
+    ngOnInit() {
+    }
+
+    noArgsExit() {
+        this.modalCtrl.dismiss();
+    }
+
+    argsExit() {
+        this.modalCtrl.dismiss({name: 'Fernando', country: 'Spain'});
+
+    }
+}
