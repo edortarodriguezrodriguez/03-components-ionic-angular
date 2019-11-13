adaacaca09d37adff9d2d22279fda4f8700fe7dc
rodriguez <e.rodriguez@ariadnagrid.com>
2019-11-13 16:27:08 +0100

    ionsearchbar

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,20 +1,25 @@
-import { Injectable } from '@angular/core';
-import { HttpClientModule, HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { ListItem } from '../model/ListItem';
+import {Injectable} from '@angular/core';
+import {HttpClientModule, HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {ListItem} from '../model/ListItem';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class DataService {
 
-  constructor(private http: HttpClient) { }
+    constructor(private http: HttpClient) {
+    }
 
-  getUser() {
-    return this.http.get('https://jsonplaceholder.typicode.com/users');
-  }
+    getUser() {
+        return this.http.get('https://jsonplaceholder.typicode.com/users');
+    }
 
-  getMenuItems() {
-    return this.http.get<ListItem[]>('/assets/data/menu.json');
-  }
+    getMenuItems() {
+        return this.http.get<ListItem[]>('/assets/data/menu.json');
+    }
+
+    getAlbums() {
+        return this.http.get<any[]>('https://jsonplaceholder.typicode.com/albums');
+    }
 }
